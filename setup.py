f51e89a317be43a3bd72e38830a4efb7bc0f12f9
Sebastian Heinlein <sebi@glatzor.de>
2007-10-21 16:54:45 +0200

    * Turn into a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="python-xrandr",
+      version="0.0.x",
+      description="Python bindings for XRandR",
+      author="Michael Vogt, Sebastian Heinlein",
+      packages=['xrandr'],
+      license = 'GNU LGPL',
+      platforms = 'posix')
+
